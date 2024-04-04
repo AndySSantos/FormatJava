74f234fb0add39094b8f09f76827f42a5bc4faac
AndySSantos <anubis.ass.origin@gmail.com>
2024-04-04 14:09:14 -0600

    Test de clase format

---
+++ src/test/java/org/example/PrintTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintTest {
+    Print p;
+    @AfterAll
+    static void inicial(){
+        p = new Print();
+    }
+
+    @Test
+    void std() {
+
+    }
+
+    @Test
+    void stdl() {
+    }
+
+    @Test
+    void format() {
+    }
+}
